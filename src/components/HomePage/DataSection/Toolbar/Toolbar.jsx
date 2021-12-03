22272d5e6095432586f9e08ea13ad707148e51dc
Amir <amir.fayzullin@gmail.com>
2021-12-03 23:56:08 +0500

    Toolbar

--- src/components/HomePage/DataSection/Toolbar/Toolbar.jsx
+++ src/components/HomePage/DataSection/Toolbar/Toolbar.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import s from "./Toolbar.module.css";
+import chartPieSlice from "./../../../../assets/ChartPieSlice.svg";
+import slidersHorizontal from "./../../../../assets/SlidersHorizontal.svg";
+import wrench from "./../../../../assets/Wrench.svg";
 
 const Toolbar = () => {
     return (
         <div className={s.wrapper}>
             <div className={s.toolsGroup}>
-                <div className={s.tool}> </div>
-                <div className={s.tool}> </div>
-                <div className={s.tool}> </div>
+                <div className={s.tool}>
+                    <img src={chartPieSlice} alt=""/>
+                </div>
+                <div className={s.tool}>
+                    <img src={slidersHorizontal} alt=""/>
+                </div>
+                <div className={s.tool}>
+                    <img src={wrench} alt=""/>
+                </div>
             </div>
         </div>
     )
