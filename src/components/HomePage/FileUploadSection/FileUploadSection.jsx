4264cd6fc84b24e1281004d1f6c9eb79d4935dfe
Amir <amir.fayzullin@gmail.com>
2021-12-03 05:50:32 +0500

    homepage file upload section implementation

--- src/components/HomePage/FileUploadSection/FileUploadSection.jsx
+++ src/components/HomePage/FileUploadSection/FileUploadSection.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./FileUploadSection.module.css";
 
 const FileUploadSection = (props) => {
+    const [file, setFile] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!file) return;
+        console.log(file);
+    };
+
     return (
         <div className={s.wrapper}>
-            file upload
+            <form onSubmit={handleSubmit}>
+                <input onChange={(e) => setFile(e.target.files[0])} type="file"/>
+                <input type="submit"/>
+            </form>
+            {/*<a href="" download>download</a>*/}
         </div>
     )
 };
